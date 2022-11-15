ccde14ff9fecee311951d0c1c868a05c33bfe895
pkkdpr <oliwia.giercza@gmail.com>
2022-11-15 10:37:12 -0500

    done, but how?

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import contactsJSON from "./contacts.json";
+import "./App.css";
+import {useState} from "react";
+function App() { 
+  const [contacts, setContacts] = useState(contactsJSON.slice(0, 5))
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>IronContacts</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+          </tr>
+        </thead>
+        {contacts.map(({id, name, pictureUrl, popularity}) => {
+          return (
+            <tbody key={id}>
+              <tr>
+                <td>
+                  <img
+                    src={pictureUrl}
+                    alt="pictureUrl"
+                    width="50px"
+                  />
+                </td>
+                <td>{name}</td>
+                <td>{popularity.toFixed(2)}</td>
+              </tr>
+            </tbody>
+          );
+        })}
+      </table>
     </div>
   );
 }
-
 export default App;
